2620b88d21b5a4016755580e4229635d09e697c1
Travis Chase <gaudeon@cpan.org>
2020-12-05 22:27:54 +0000

    filter_number_list runs through the numbers

--- src/app.py
+++ src/app.py
@@ -1,12 +1,40 @@
 def get_number_list():
   file = open("data/1_1_inputs.txt", "r")
   lines = file.readlines()
-  
-  number_list = []
-  for line in lines:
-    number_list.append(int(line))
 
-  return number_list
+  new_number_list = []
+  for line in lines:
+    new_number_list.append(int(line))
+
+  return new_number_list
+# end of get_number_list
+
+def filter_number_list(the_number_list, criteria_def):
+  # go through all numbers, store in first variable
+  for first_pos in range(0, len(the_number_list)):
+    # go through all numbers, store in second variable
+    for second_pos in range(0, len(the_number_list)):
+        if first_pos == second_pos:
+          continue
+
+        first_number = the_number_list[first_pos]
+        second_number = the_number_list[second_pos]
+        
+        print(first_pos, second_pos)
+        pass
+  # add numbers stored in first and second variable
+  # if a pair equals 2020 then keep them
+  # if a pair doesn't equal 2020 continue 
+  pass
+# end of filter_number_list
+
+def is_sum_2020(the_first_number, the_second_number):
+  pass
+# end of is_sum_2020
+
+# main
 
 number_list = get_number_list()
 print(number_list)
+
+filter_number_list(number_list, is_sum_2020)
