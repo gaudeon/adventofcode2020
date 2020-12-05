f70058947257266fd1c5439ba294a279e1e44213
Travis Chase <gaudeon@cpan.org>
2020-12-05 21:52:36 +0000

    number list now really returns a list of numbers

--- src/app.py
+++ src/app.py
@@ -1,7 +1,12 @@
 def get_number_list():
   file = open("data/1_1_inputs.txt", "r")
   lines = file.readlines()
-  return lines
+  
+  number_list = []
+  for line in lines:
+    number_list.append(int(line))
+
+  return number_list
 
 number_list = get_number_list()
 print(number_list)
